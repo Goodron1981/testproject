5341711f8e3738597a15a5ce602806744625855b
Vetal <Ponamarev7@gmail.com>
2018-03-15 18:05:47 +0200

    добавлеем выгрузку рота
    начало выгрузки рота

--- runapp/rota/load_isue.py
+++ runapp/rota/load_isue.py
@@ -1,17 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-import xml
+import lxml
 from testpage.models import Accounts
 from runapp.models import Isue
 from datetime import datetime
 
 
-def load_isue_sape():
+def load_isue_rota():
     now = datetime.now()
     #param a дозапись
     f = open('runapp\logfile.txt', 'a')
     f.write('Function load_isue_rota: ' + datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S") + '\n')
-
 
     proxies = {
       'http': 'http://10.18.7.6:3128',
@@ -19,43 +18,53 @@ def load_isue_sape():
     }
 
     acount_list = Accounts.objects.all()
+
     for user in acount_list:
         user_name = user.name
         apikey = user.apikey_rota
-        url = "https://api.rotapost.ru/Task/Webmaster?Status=ToDo&ApiKey=" + apikey
-        cookies = user.cookies_sape
-        if cookies != 'None':
-            #payload = "<?xml version=\"1.0\"?>\r\n<methodCall>\r\n  <methodName>sape_pr.login</methodName>\r\n  <params>\r\n    <param>\r\n        <value><string>" + token + "</string></value>\r\n    </param>\r\n  </params>\r\n</methodCall>"
-            payload = "<?xml version=\"1.0\"?>\r\n<methodCall>\r\n  <methodName>sape_pr.site.adverts</methodName>\r\n  <params>\r\n    <param>\r\n        <value><struct><member><name>status_codes</name><value><int>5</int></value>\r\n    </member></struct></value>\r\n  </param>\r\n  <param><value><int>1</int></value>\r\n  </param>\r\n  </params>\r\n</methodCall>"
-
+        if apikey != 'None':
+            url = "https://api.rotapost.ru/Task/Webmaster?Status=ToDo&ApiKey=" + apikey
+            #print(url + '\n')
             headers = {
                 'Content-Type': "text/xml",
                 'Accept-Charset': "utf-8",
-                'Cache-Control': "no-cache",
-                'Cookie': cookies
+                'Cache-Control': "no-cache"
                 }
-            response = requests.post(url=url, data=payload, headers=headers, proxies=proxies)
+            response = requests.get(url=url, headers=headers, proxies=proxies)
             #response = requests.post(url=url, data=payload, headers=headers)
             #print('Текст ответа: ', response.text)
 
-            trace = BeautifulSoup(response.text, "xml")
-            root = trace.find_all('data')[0]
-            isuecount = root.contents
-            for isue in isuecount:
-                book = isue.find('struct')
-                isuenum = book.contents[0].find('value').get_text()
-                isuetype = book.contents[1].find('value').get_text()
-                site_link = book.contents[3].find('value').get_text()
-                createdate = book.contents[23].find('value').get_text()
-                ancor_href = book.contents[5].find('struct').contents[1].find('value').get_text()
-                ancor_text = book.contents[5].find('struct').contents[2].find('value').get_text()
-                ancor_tag = '<a href="'+ ancor_href +'">' + ancor_text + '</a>'
-                c = Isue.objects.count()
-                #print(c)
-                site_link = site_link.split('//')[1]
-                b = Isue(num = c+1, id_isue = isuenum, type_isue = isuetype, site_platform = site_link, date_create = createdate, anchor1 = ancor_tag, anchor1_url = ancor_href, anchor1_text = ancor_text, user_platform = user_name, platform_name = 'Sape')
-                #b.currently = "shift"
-                b.save()
-                f.write(user_name + ': ' + isuenum + ': ' + isuetype + ': ' + site_link + ': ' + createdate + ': ' + ancor_href + ': ' + ancor_text + '\n')
+            trace = BeautifulSoup(response.text, "lxml")
+            result_block = trace.find('success')
+            validate = result_block.get_text()
+            if validate:
+                idisues = trace.find_all('id')
+                typeisues = trace.find_all('type')
+                siteisues = trace.find_all('site')
+                cuisues = trace.find_all('checkurl')
+                cdisues = trace.find_all('createdate')
+                print('\n')
+                for isue in idisue:
+                    print(isue.get_text())
+                    #print(str(idisue), + '\n')
+                    '''
+                    isuenum = book.contents[0].find('value').get_text()
+                    isuetype = book.contents[1].find('value').get_text()
+                    site_link = book.contents[3].find('value').get_text()
+                    createdate = book.contents[23].find('value').get_text()
+                    ancor_href = book.contents[5].find('struct').contents[1].find('value').get_text()
+                    ancor_text = book.contents[5].find('struct').contents[2].find('value').get_text()
+                    ancor_tag = '<a href="'+ ancor_href +'">' + ancor_text + '</a>'
+                    c = Isue.objects.count()
+                    #print(c)
+                    #site_link = site_link.split('//')[1]
+                    b = Isue(num = c+1, id_isue = isuenum, type_isue = isuetype, site_platform = site_link, date_create = createdate, anchor1 = ancor_tag, anchor1_url = ancor_href, anchor1_text = ancor_text, user_platform = user_name, platform_name = 'Sape')
+                    #b.currently = "shift"
+                    b.save()
+                    f.write(user_name + ': ' + isuenum + ': ' + isuetype + ': ' + site_link + ': ' + createdate + ': ' + ancor_href + ': ' + ancor_text + '\n')
+                    '''
+            else:
+                print(user_name + ': Error autorization')
+                f.write(user_name + ': Error autorization' + '\n')
     f.write('\n')
     f.close()
